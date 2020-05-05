9bafa37694edaa8a1b849d961787c1c34c3346b1
BelyashMatvey <53214557+BelyashMatvey@users.noreply.github.com>
2020-05-05 11:06:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import apiai, json
+import pyowm
+owm=pyowm.OWM('0f63162b7e1fdf0e85e8b1476b34f2de', language = 'ru')
 updater = Updater(token='1196701841:AAHB12rfu953tk4Wl55FSDN8VlPtj7RGxXM') # Токен API к Telegram
 dispatcher = updater.dispatcher
 # Обработка команд
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 def textMessage(bot, update):
-    request = apiai.ApiAI('4a6d212e347b41498df38535311f7621').text_request() # Токен API к Dialogflow
+    request = apiai.ApiAI('4b89cdb6c8b94e568117a949cd938a5e').text_request() # Токен API к Dialogflow
     request.lang = 'ru' # На каком языке будет послан запрос
     request.session_id = 'BatlabAIBot' # ID Сессии диалога (нужно, чтобы потом учить бота)
     request.query = update.message.text # Посылаем запрос к ИИ с сообщением от юзера
@@ -17,12 +19,16 @@ def textMessage(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
         bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+def weather_start(bot,update):
+    bot.send_message(chat_id=update.message.chat_id, text='В каком городе хотите узнать погоду?')
 # Хендлеры
+weather_start_command_handler=CommandHandler('weather',weather_start)
 start_command_handler = CommandHandler('start', startCommand)
 text_message_handler = MessageHandler(Filters.text, textMessage)
 # Добавляем хендлеры в диспетчер
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(text_message_handler)
+dispatcher.add_handler(weather_start_command_handler)
 # Начинаем поиск обновлений
 updater.start_polling(clean=True)
 # Останавливаем бота, если были нажаты Ctrl + C
